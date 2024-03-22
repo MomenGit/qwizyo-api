8f1d2b90e949c8b6f966cbf2dbfc9e53765a79a5
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2024-03-22 23:00:43 +0200

    Prepare rpoject structure

---
+++ api/__init__.py
@@ -0,0 +1,16 @@
+import os
+from flask import Flask
+from flask_cors import CORS
+
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+    )
+
+    CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
+    return app
