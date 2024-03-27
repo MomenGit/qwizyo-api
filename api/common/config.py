2896af902b38f4ca489f3b480c070b85f214aed9
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2024-03-27 13:23:24 +0200

    Add docs

--- api/common/config.py
+++ api/common/config.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
-""""""
+"""Defines App Configurations for different environments"""
 import os
-from dotenv import load_dotenv, dotenv_values
+from dotenv import load_dotenv
 
 # loading variables from .env file
 load_dotenv()
 
 
 class Config:
-    """"""
+    """Default Configs"""
     DEBUG = False
     SECRET_KEY = os.getenv("SECRET_KEY")
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
@@ -16,14 +16,14 @@ class Config:
 
 
 class DevelopmentConfig(Config):
-    """"""
+    """Dev Configs"""
     DEBUG = True
     MONGODB_URI = os.getenv("MONGODB_URI_DEV")
     # Add development-specific configuration variables
 
 
 class ProductionConfig(Config):
-    """"""
+    """Productions Configs"""
     DEBUG = False
     MONGODB_URI = os.getenv("MONGODB_URI")
     # Add production-specific configuration variables
