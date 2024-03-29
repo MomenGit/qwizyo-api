34c5ce80820d3638307219e0ab8009ff291be1e7
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2024-03-29 15:10:08 +0200

    Add assignments routes

--- api/common/utils.py
+++ api/common/utils.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Define utils functions"""
 from api.resources import (
+    assignment_resource,
     auth_resource,
     group_resource,
     quiz_resource,
@@ -12,15 +13,28 @@ from api.resources import (
 def setup_routes(api):
     """Map Resources to routes"""
     api.add_resource(user_resource.User, '/users/<string:id>')
+
+    # Auth Routes
     api.add_resource(auth_resource.RegisterAuth, '/auth/register')
     api.add_resource(auth_resource.LoginAuth, '/auth/login')
     api.add_resource(auth_resource.TokenRefresh, '/auth/refresh')
+
+    # Status Route
     api.add_resource(miscellaneous_resource.Status, '/ping')
+
+    # Group Routes
     api.add_resource(group_resource.Groups, '/groups')
     api.add_resource(group_resource.Group, '/groups/<string:id>')
+
+    # Quiz Routes
     api.add_resource(quiz_resource.QuizList, '/quizzes')
     api.add_resource(quiz_resource.Quiz, '/quizzes/<string:id>')
-    """ api.add_resource('/quizzes')
-    api.add_resource('/quizzes/<str:id>')
-    api.add_resource('/assignments')
-    api.add_resource('/assignments/<str:id>') """
+
+    # Assignment Routes
+    api.add_resource(assignment_resource.AssignmentList, '/assignments')
+    api.add_resource(assignment_resource.Assignment,
+                     '/assignments/<string:id>')
+    api.add_resource(assignment_resource.QuizAssignmentsList,
+                     '/quizzes/<string:id>/assignments')
+    api.add_resource(assignment_resource.GroupAssignmentsList,
+                     '/groups/<string:id>/assignments')
